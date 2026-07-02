465febc51f8ac63ac8150d82ee561040672cf78a
Tom Chen <tomchen.org@gmail.com>
2026-07-03 01:31:41 +0200

    chore: release v1.0.0

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,7 @@ import {
 	parse,
 } from "./parser";
 
-const VERSION = "1.0.0-beta.0"; // vbt-version
+const VERSION = "1.0.0"; // vbt-version
 
 function formatSize(bytes: number): string {
 	if (bytes < 1024) return `${bytes} B`;
